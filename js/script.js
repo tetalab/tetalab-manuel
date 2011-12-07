dda8fbc5abd9b067c5b9b2afaf0bddaad75b2774
Alexandre Girard <git@alexgirard.com>
2011-12-07 10:13:23 +0100

    add map for manger/boire

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 /* Author:
 
 */
+$(document).ready(function() {
+  $("#map a").mouseover(function(){
+    $("#location-pin").attr("style", $(this).data("position"));
+  });
 
-
-
-
+  $("#map a").mouseout(function(){
+    $("#location-pin").attr("style", "position:relative;top:-400px;left:-660px;");
+  });
+});
