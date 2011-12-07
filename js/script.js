6717ec46c3e7318e9681826adf7b2073f36cac00
Alexandre Girard <git@alexgirard.com>
2011-12-07 10:58:38 +0100

    add popover

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@
 
 */
 $(document).ready(function() {
+
+  $("#map a").popover({html: true});
+
   $("#map a").mouseover(function(){
     $("#location-pin").attr("style", $(this).data("position"));
+    $(this).popover('show');
   });
 
   $("#map a").mouseout(function(){
+    $(this).popover('hide');
     $("#location-pin").attr("style", "position:relative;top:-400px;left:-660px;");
   });
 });
